249d4340248be287ea5a4ae8f1e61073f6b3957a
Jakub <jakub.kukier01@gmail.com>
2026-06-19 01:44:54 +0200

    Solution

--- services/movie.py
+++ services/movie.py
@@ -25,7 +25,7 @@ def get_movies(
     return Movie.objects.all().distinct()
 
 
-def get_movie_by_id(movie_id: int):
+def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
@@ -34,7 +34,7 @@ def create_movie(
         movie_description: str,
         genres_ids: List[int] = None,
         actors_ids: List[int] = None
-):
+) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,13 +1,13 @@
 from django.db.models import QuerySet
 
-from db.models import MovieSession
+from db.models import MovieSession, Movie
 
 
 def create_movie_session(
         movie_show_time: str,
         movie_id: int,
         cinema_hall_id: int
-):
+) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
         cinema_hall_id=cinema_hall_id,
@@ -21,7 +21,7 @@ def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     return MovieSession.objects.all()
 
 
-def get_movie_session_by_id(movie_session_id: int):
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
@@ -30,7 +30,7 @@ def update_movie_session(
         show_time: str = None,
         movie_id: int = None,
         cinema_hall_id: int = None,
-):
+) -> MovieSession:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time is not None:
         movie_session.show_time = show_time
